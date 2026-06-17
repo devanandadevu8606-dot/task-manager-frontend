efc5212a6c28aa5854a21da7693b37fba2a0a196
devanandadevu8606 <devanandadevu8606@gmail.com>
2026-06-17 12:08:39 +0530

    final fix for vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import Profile from "./pages/profile.jsx";
 import Login from "./pages/login.jsx";
+import Profile from "./pages/profile.jsx";
 import Register from "./pages/register.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 import Tasks from "./pages/tasks.jsx";
-  import Layout from "./components/layout"; 
-  import ProtectedRoute from "./utils/protectedRoute";
+
+import Layout from "./components/Layout.jsx";
+import ProtectedRoute from "./utils/ProtectedRoute.jsx";
    function App() { return ( <BrowserRouter> <Routes> {/* PUBLIC ROUTES */} <Route path="/" element={<Login />} /> <Route path="/login" element={<Login />} /> <Route path="/register" element={<Register />} /> {/* PROTECTED ROUTES */} <Route path="/dashboard" element={ <ProtectedRoute> <Layout> <Dashboard /> </Layout> </ProtectedRoute> } /> <Route path="/tasks" element={ <ProtectedRoute> <Layout> <Tasks /> </Layout> </ProtectedRoute> } /> <Route path="/profile" element={ <ProtectedRoute> <Layout> <Profile /> </Layout> </ProtectedRoute> } /> </Routes> </BrowserRouter> ); } export default App;
