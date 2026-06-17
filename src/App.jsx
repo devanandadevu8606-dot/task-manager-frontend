7ed0f922cf044294af7667b4cb6d14eea084501b
devanandadevu8606 <devanandadevu8606@gmail.com>
2026-06-17 11:54:34 +0530

    fix App.jsx deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import Profile from "./pages/Profile.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Tasks from "./pages/Tasks.jsx";
+import Profile from "./pages/profile.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+import Tasks from "./pages/tasks.jsx";
   import Layout from "./components/Layout"; 
   import ProtectedRoute from "./utils/ProtectedRoute";
    function App() { return ( <BrowserRouter> <Routes> {/* PUBLIC ROUTES */} <Route path="/" element={<Login />} /> <Route path="/login" element={<Login />} /> <Route path="/register" element={<Register />} /> {/* PROTECTED ROUTES */} <Route path="/dashboard" element={ <ProtectedRoute> <Layout> <Dashboard /> </Layout> </ProtectedRoute> } /> <Route path="/tasks" element={ <ProtectedRoute> <Layout> <Tasks /> </Layout> </ProtectedRoute> } /> <Route path="/profile" element={ <ProtectedRoute> <Layout> <Profile /> </Layout> </ProtectedRoute> } /> </Routes> </BrowserRouter> ); } export default App;
