d35f1e8093a63c739e18ee6bf8327c406fd8945a
devanandadevu8606 <devanandadevu8606@gmail.com>
2026-06-17 12:03:06 +0530

    final fix for vercel case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,6 @@ import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 import Tasks from "./pages/tasks.jsx";
-  import Layout from "./components/Layout"; 
-  import ProtectedRoute from "./utils/ProtectedRoute";
+  import Layout from "./components/layout"; 
+  import ProtectedRoute from "./utils/protectedRoute";
    function App() { return ( <BrowserRouter> <Routes> {/* PUBLIC ROUTES */} <Route path="/" element={<Login />} /> <Route path="/login" element={<Login />} /> <Route path="/register" element={<Register />} /> {/* PROTECTED ROUTES */} <Route path="/dashboard" element={ <ProtectedRoute> <Layout> <Dashboard /> </Layout> </ProtectedRoute> } /> <Route path="/tasks" element={ <ProtectedRoute> <Layout> <Tasks /> </Layout> </ProtectedRoute> } /> <Route path="/profile" element={ <ProtectedRoute> <Layout> <Profile /> </Layout> </ProtectedRoute> } /> </Routes> </BrowserRouter> ); } export default App;
