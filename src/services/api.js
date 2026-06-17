3e5473a81a4b00812b3040fc986fb8f92bf501f5
devanandadevu8606 <devanandadevu8606@gmail.com>
2026-06-17 10:46:35 +0530

    production api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-backend-oj2f.onrender.com",
 });
 
 // REQUEST INTERCEPTOR (ADD TOKEN AUTOMATICALLY)
